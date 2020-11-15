40b14ee181ce7e78165bacc58c776a233aa34a18
Stefan Eng <eng_steff@outlook.com>
2020-11-15 12:43:58 +0100

    Add whitespace between app.py methods.

--- project_webber/app.py
+++ project_webber/app.py
@@ -3,12 +3,15 @@ from flask import Flask, render_template
 from project_webber.utils import db_query, db_commit, db_execute
 from project_webber.utils import dice_create
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
   add_stuff()
   return render_template('index.html')
+
 
 def add_stuff():
   dice1 = dice_create(range(1,7))
