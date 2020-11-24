0f5787036d0f907dd81315319ee1fd7a17dcb992
Stefan Eng <eng_steff@outlook.com>
2020-11-24 21:12:55 +0100

    Hello Triangle working.

--- project_webber/static/js/main.js
+++ project_webber/static/js/main.js
@@ -1,1 +1,104 @@
-alert("HELLO");
+let canvas = document.querySelector("#c");
+let gl = canvas.getContext("webgl2");
+
+if (!gl) {
+  alert("Can't load webgl2 context.");
+}
+
+let vertexShaderSource = `#version 300 es
+in vec4 a_position;
+void main() {
+  gl_Position = a_position;
+}`;
+
+let fragmentShaderSource = `#version 300 es
+precision highp float;
+out vec4 outColor;
+
+void main() {
+  outColor = vec4(1, 0, 0.5, 1);
+}`;
+
+function createShader(gl, type, source) {
+  let shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  let success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (success) {
+    return shader;
+  }
+
+  console.log(gl.getShaderInfoLog(shader));
+  gl.deleteShader(shader);
+}
+
+let vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+let fragmentShader = createShader(
+  gl, gl.FRAGMENT_SHADER, fragmentShaderSource
+);
+
+function createProgram(gl, vertexShader, fragmentShader) {
+  let program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  let success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (success) {
+    return program;
+  }
+
+  console.log(gl.getProgramInfoLog(program));
+  gl.deleteProgram(program);
+}
+
+let program = createProgram(gl, vertexShader, fragmentShader);
+let positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
+let positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+let positions = [
+  0, 0,
+  0, 0.5,
+  0.7, 0,
+];
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+let vao = gl.createVertexArray();
+gl.bindVertexArray(vao);
+gl.enableVertexAttribArray(positionAttributeLocation);
+
+let size = 2;
+let type = gl.FLOAT;
+let normalize = false;
+let stride = 0;
+let offset = 0;
+gl.vertexAttribPointer(
+  positionAttributeLocation,
+  size,
+  type,
+  normalize,
+  stride,
+  offset
+);
+
+function resize(canvas) {
+  let displayWidth = canvas.clientWidth;
+  let displayHeight = canvas.clientHeight;
+
+  if (canvas.width !== displayWidth || canvas.height !== displayHeight) {
+    canvas.width = displayWidth;
+    canvas.height = displayHeight;
+  }
+}
+
+resize(gl.canvas);
+gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+gl.clearColor(0, 0, 0, 0);
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+gl.useProgram(program);
+gl.bindVertexArray(vao);
+
+let primitiveType = gl.TRIANGLES;
+let count = 3;
+gl.drawArrays(primitiveType, offset, count);
